0d4e8444d9d3cfba2ba72e211483d22524dc334e
adelon <22380201+adelon@users.noreply.github.com>
2026-02-12 23:26:45 +0100

    Update Unit.hs

--- source/Test/Unit.hs
+++ source/Test/Unit.hs
@@ -3,7 +3,6 @@ module Test.Unit where
 
 import Test.Tasty
 import Test.Tasty.HUnit
-import Test.Unit.Nominal qualified as Nominal
 import Test.Unit.Symdiff qualified as Symdiff
 import Test.Unit.Syntax qualified as Syntax
 
@@ -11,7 +10,6 @@ unitTests :: TestTree
 unitTests = testGroup "unit tests"
     [testCase "filter" filtersWell
     , Syntax.unitTests  -- include the Syntax.DeBruijn tests
-    , Nominal.unitTests
     ]
 
 
